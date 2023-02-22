2929e3cd87a49fe1847bffcf4b296e79eb2415bf
Ed Kraus <ekraus0227@gmail.com>
2023-02-22 21:48:26 +0300

    Initial Commit

---
+++ AFSpaceA/AFSpaceAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFSpaceAApp.swift
+//  AFSpaceA
+//
+//  Created by Ed Kraus on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AFSpaceAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
