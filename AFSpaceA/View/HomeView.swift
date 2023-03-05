baafc86a224004cd47cc8ee724273381f666a288
sourkraut1991 <69095925+sourkraut1991@users.noreply.github.com>
2023-03-05 08:20:21 +0300

    Merge branch 'main' of https://github.com/sourkraut1991/AFSpaceA

--- AFSpaceA/View/HomeView.swift
+++ AFSpaceA/View/HomeView.swift
@@ -3,28 +3,96 @@
 //  AFSpaceA
 //
 //  Created by Ed Kraus on 3/4/23.
-//
 
 import SwiftUI
 
 struct HomeView: View {
+    private var eucome: [Locations] = Locations.eucomBases
+    private var conus: [Locations] = Locations.conusBases
     
     @ObservedObject var base = ContentModel()
     
     var body: some View {
-        NavigationStack {
-            
-            List(base.bases){ a in
-                NavigationLink(a.name) {
-                    DetailView(base: a)
+        VStack {
+            NavigationStack {
+                
+                List(eucome){ a in
+                    NavigationLink(a.name){
+                        DetailView(base: a)
+                    }
+                    .navigationBarTitle("Regions")
                 }
                 
             }
+           
+            
         }
         
-        .navigationBarTitle("Bases")
     }
+    
 }
+//    var bases = [
+//        Locations(Commercial: base.Commercial, DSN: base?.DSN, email: base.email, id: base.id, image: base.image, name: base.name, region: base?.region, website: base.website),
+//        Locations(Commercial: base.Commercial, DSN: base?.DSN, email: base.email, id: base.id, image: base.image, name: base.name, region: base?.region, website: base.website)
+//    ]
+//
+//    // Declare what you want!
+//    // You want an array of EUCOM bases.
+//    var eucomBases: [Locations] {
+//        bases.filter{$0.region == "EUCOM"} // <-- find region within your bases array
+//    }
+//
+//    // Declear what you want!
+//    // You want an array of CONUS bases.
+//    var conusBases: [Locations] {
+//        bases.filter{$0.region == "CONUS"} // same here.
+//    }
+
+//        }
+
+
+
+
+//        // NavigationView to go to detail view
+//        NavigationView {
+//
+//            ScrollView {
+////                List(base.bases) { a in
+////                    // VStack to group all the cards
+////
+////                    NavigationLink(destination: DetailView(base: a)) {
+////                        Text(a.name)
+////                    }
+//
+////                     Card for each base
+////
+////
+////                                                // Link to detail view
+//                ForEach(base.bases) { a in
+////                    List {
+////                        Text(a.name)
+////                    }
+//                                                NavigationLink(
+//                                                    destination: DetailView(base: a),
+//                                                    label: {
+//
+//                                                        // Each base card in the scrollview
+//                                                        CardView(base: a)
+//                                                    })
+//                    }
+//
+//                }
+//            .navigationBarTitle("Bases")
+//
+//            }
+//
+//
+//        }
+//
+
+//    }
+//
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
--- AFSpaceA/View/RootView.swift
+++ AFSpaceA/View/RootView.swift
@@ -5,95 +5,32 @@
 //  Created by Ed Kraus on 2/22/23.
 //
 
+//
+
 import SwiftUI
 
 struct RootView: View {
-    private var eucome: [Locations] = Locations.eucomBases
-    private var conus: [Locations] = Locations.conusBases
     
     @ObservedObject var base = ContentModel()
+    @State var selectedTab: Tabs = .home
     
     var body: some View {
-        VStack {
-            NavigationStack {
+        ZStack {
+           
+            
+            VStack {
                 
-                List(conus){ a in
-                    NavigationLink(a.name){
-                        DetailView(base: a)
-                    }
-                    //                    List(eucome){ a in
-                    //                        NavigationLink(a.name){
-                    //                            DetailView(base: a)
-                    //                        }
-                    .navigationBarTitle("Regions")
+                switch selectedTab {
+                case .home:
+                    HomeView()
+                case .about:
+                    AboutView()
                 }
                 
+                Spacer()
+                CustomTabBar(selectedTab: $selectedTab)
             }
-            CustomTabBar()
-            
-        }
-        
+                
+            }
     }
-    
 }
-//    var bases = [
-//        Locations(Commercial: base.Commercial, DSN: base?.DSN, email: base.email, id: base.id, image: base.image, name: base.name, region: base?.region, website: base.website),
-//        Locations(Commercial: base.Commercial, DSN: base?.DSN, email: base.email, id: base.id, image: base.image, name: base.name, region: base?.region, website: base.website)
-//    ]
-//
-//    // Declare what you want!
-//    // You want an array of EUCOM bases.
-//    var eucomBases: [Locations] {
-//        bases.filter{$0.region == "EUCOM"} // <-- find region within your bases array
-//    }
-//
-//    // Declear what you want!
-//    // You want an array of CONUS bases.
-//    var conusBases: [Locations] {
-//        bases.filter{$0.region == "CONUS"} // same here.
-//    }
-
-//        }
-
-
-
-
-//        // NavigationView to go to detail view
-//        NavigationView {
-//
-//            ScrollView {
-////                List(base.bases) { a in
-////                    // VStack to group all the cards
-////
-////                    NavigationLink(destination: DetailView(base: a)) {
-////                        Text(a.name)
-////                    }
-//
-////                     Card for each base
-////
-////
-////                                                // Link to detail view
-//                ForEach(base.bases) { a in
-////                    List {
-////                        Text(a.name)
-////                    }
-//                                                NavigationLink(
-//                                                    destination: DetailView(base: a),
-//                                                    label: {
-//
-//                                                        // Each base card in the scrollview
-//                                                        CardView(base: a)
-//                                                    })
-//                    }
-//
-//                }
-//            .navigationBarTitle("Bases")
-//
-//            }
-//
-//
-//        }
-//
-
-//    }
-//
